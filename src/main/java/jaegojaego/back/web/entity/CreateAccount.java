68af4804700d73780ff18a542e211a5e4ab924c1
JinTaekLim <jintaek019@gmail.com>
2024-01-21 17:06:01 +0900

    getter setter 변경

--- src/main/java/jaegojaego/back/web/entity/CreateAccount.java
+++ src/main/java/jaegojaego/back/web/entity/CreateAccount.java
@@ -2,93 +2,32 @@ package jaegojaego.back.web.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 @Entity
 public class CreateAccount {
 
-    @Id
+    @Id @Getter @Setter
     private String businessNumber;  //사업자 등록 번호
+    @Getter @Setter
     private String businessname;    //대표자 성명
+    @Getter @Setter
     private LocalDate openDate;     //개업일자
+    @Getter @Setter
     private String shopName;    //상호
+    @Getter @Setter
     private String shopAdress;  //매장주소
+    @Getter @Setter
     private String email;   //이메일
+    @Getter @Setter
     private String phoneNumber; //연락처
+    @Getter @Setter
     private String id;  // 아이디
+    @Getter @Setter
     private String pw;  // 비밀번호
 
-    public String getBusinessNumber() {
-        return businessNumber;
-    }
-
-    public void setBusinessNumber(String businessNumber) {
-        this.businessNumber = businessNumber;
-    }
-
-    public String getBusinessname() {
-        return businessname;
-    }
-
-    public void setBusinessname(String businessname) {
-        this.businessname = businessname;
-    }
-
-    public LocalDate getOpenDate() {
-        return openDate;
-    }
-
-    public void setOpenDate(LocalDate openDate) {
-        this.openDate = openDate;
-    }
-
-    public String getShopName() {
-        return shopName;
-    }
-
-    public void setShopName(String shopName) {
-        this.shopName = shopName;
-    }
-
-    public String getShopAdress() {
-        return shopAdress;
-    }
-
-    public void setShopAdress(String shopAdress) {
-        this.shopAdress = shopAdress;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPw() {
-        return pw;
-    }
-
-    public void setPw(String pw) {
-        this.pw = pw;
-    }
 
 
 }
